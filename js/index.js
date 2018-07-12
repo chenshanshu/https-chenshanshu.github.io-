1305d431c6701b8662298e97dcbacd7826e414d2
ShanShu <shanshu@dajiahuton.com>
2018-07-12 13:33:39 +0800

    2018/7/12

--- js/index.js
+++ js/index.js
@@ -15,9 +15,36 @@
                 particlesNumber:"150"
             });
 
-        }
-        lizi();
-        $(window).resize(function () {          //当浏览器大小变化时
-		lizi();
-        });
+        };
+        // lizi();
+        // $(window).resize(function () {          //当浏览器大小变化时
+        // lizi();
+        // });
+        $('form').submit(function (e) {
+            var event = e || window.event,
+                $form = $(this);
+            event.preventDefault();
+
+            var formData = $form.serializeArray();
+
+            formData = {
+                mobile:$form.context.phone.value,
+                tpl_id:84516,
+                tpl_value:"#code#="+$form.context.message.value,
+                key:"ca3bfb4443efb89e1f4c02c2b57b342a"
+            };
+            $.ajax({
+                type:$form.attr('method'),
+                url:$form.attr('action'),
+                data:formData,
+                dataType:'jsonp'
+            }).done(function (data) {
+                console.log(data);
+
+            }).fail(function (err) {
+                console.log(err);
+
+            });
+            return ;
+        })
     });
